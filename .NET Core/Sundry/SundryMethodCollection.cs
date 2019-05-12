0bbffb26427f6affe12d58e3bba4b9ca25e9523f
梁建溢 <liangjianyi.com@outlook.com>
2019-05-12 07:17:14 -0400

    Add Sundry project and SundryMethodCollection class

---
+++ .NET Core/Sundry/SundryMethodCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Janyee.Utilty {
+    public static class SundryMethodCollection {
+        public static string BytesToStringArray(byte[] source) {
+            string[] temp = (from n in source select n.ToString("X")).ToArray();
+            string text = string.Empty;
+            text += "{ ";
+            for (int i = 0; i < temp.Length; i++) {
+                text += string.IsNullOrEmpty(temp[i]) ? "null" : "\"" + temp[i] + "\"";
+                text += i == temp.Length - 1 ? " " : ", ";
+            }
+            text += "}";
+            return text;
+        }
+    }
+}
